30be203760d442a2a4de02c0d4f7ef40733d4d19
inoyka <simonmarkholland@gmail.com>
2018-02-07 13:49:59 +0700

    Added file under trial/

--- cks/first_app/urls.py
+++ cks/first_app/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from first_app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('trial/', include('first_app.urls')),
-    path('admin/', admin.site.urls),
 ]
